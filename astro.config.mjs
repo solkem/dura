efee6a24a9baedda78a2b006946942563dd2cde6
solkem <solomonkembo@gmail.com>
2025-12-13 17:47:18 -0500

    fix(build): switch to static output and static vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
